42ac290f5616411e84d4795e64a6017b25392214
Hew Wolff <hew@hewwolff.org>
2023-06-27 22:49:26 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,75 @@
+// Create web server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+// Create express application
+const app = express();
+
+// Use body parser middleware
+app.use(bodyParser.json());
+
+// Use cors middleware
+app.use(cors());
+
+// Create comments object
+const commentsByPostId = {};
+
+// Create route for fetching comments
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+// Create route for creating comments
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content, status: 'pending' });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+// Create route for receiving events from event bus
+app.post('/events', (req, res) => {
+    console.log('Event received:', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        // Send event to event bus
+        axios.post('http://event-bus-srv:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
+            }
+        });
+    }
+
+    res.send({});
+});
+
+// Listen on port 4001
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
